4abd8b6778a9a92b61a72c3fdf3a5bc390f02a8a
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-11 21:59:40 -0800

    added getbyID and add methods to the comments api

--- api/comments/comments-model.js
+++ api/comments/comments-model.js
@@ -3,12 +3,17 @@ const config = require("../../knexfile.js");
 const db = knex(config.development);
 
 module.exports = {
-  find,
+  get,
+  getByID,
   add,
 };
 
-function find() {
+function get() {
   return db("comments");
+}
+
+function getByID(id) {
+  return db("comments").where({ id }).first();
 }
 
 function add(comment) {
--- api/comments/comments-router.js
+++ api/comments/comments-router.js
@@ -3,7 +3,7 @@ const Comments = require("./comments-model");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  Comments.find()
+  Comments.get()
     .then((comments) => {
       res.status(200).json(comments);
     })
@@ -11,6 +11,23 @@ router.get("/", (req, res) => {
       console.log(error);
       res.status(500).json({
         message: "Error retrieving the comments",
+      });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Comments.getByID(req.params.id)
+    .then((comment) => {
+      if (comment) {
+        res.status(200).json(comment);
+      } else {
+        res.status(404).json({ message: "Comment not found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the comment",
       });
     });
 });
